9c4856f561f521ffcb296ab4af41bb03123dffef
Ziyi Yang <ziyiyang332@gmail.com>
2021-08-01 17:15:11 +0200

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+modul.export = {
+  printWidth :  140 ,
+  tabBredd :  2 ,
+  useTabs :  false ,
+  semi :  sant ,
+  singleQuote :  sant ,
+  trailingComma :  'alla' ,
+  bracketSpacing :  true ,
+  jsxBracketSameLine :  falskt ,
+  arrowParens :  'undvik' ,
+};
